abc6f74368eca34d061a76c7e721f2c0875a1a25
chooch620 <matthew.z.ruiz@gmail.com>
2020-07-21 23:41:51 -0400

    Now contains logo

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-import {Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, Image} from 'react-native';
+import {DEVICE_WIDTH} from "../../styles";
 
 type SplashProps = {};
 
 export const Splash = (props: SplashProps) => {
-  return <Text>Splash</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/images/logo.png')}
+      />
+    </SafeAreaView>
+  );
 };
+
+const logoSize = DEVICE_WIDTH / 1.75;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+
+  logo: {
+    width: logoSize,
+    height: logoSize,
+    alignSelf: 'center',
+  },
+});
